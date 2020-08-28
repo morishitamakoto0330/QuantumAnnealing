f6281e60e37c47b7640d8aaacbf70a3b11b5cf7c
morishitamakoto0330 <morishita0330@gmail.com>
2020-08-28 19:25:03 +0900

    [update] execute CMOS Annealing in Web API v2

--- CMOSAnnealing/test.py
+++ CMOSAnnealing/test.py
@@ -3,10 +3,14 @@ import urllib.parse
 import json
 
 access_token = '046eaee43c3a068433eb6a1c89a9c8fc'
-url = 'https://annealing-cloud.com/api/v1/solve'
-#model = [[0,0,0,0,1],[0,1,0,0,-1]]
+headers = {'Authorization': 'Bearer '+ access_token}
+#url = 'https://annealing-cloud.com/api/v1/solve'
+url = 'https://annealing-cloud.com/api/v2/solve'
 
 
+
+# Web API version 1
+'''
 params = {
   "num_executions": 5,
   "model": [
@@ -29,6 +33,44 @@ params = {
     "averaged_energy": True
   }
 }
+'''
+
+# Web API version 2
+params = {
+	"type": 3,
+  "num_executions": 5,
+  "model": [
+    [0,0,0,0,4],
+    [0,1,0,2,-1],
+    [1,0,1,1,1],
+    [1,1,1,2,1],
+    [2,0,2,1,1],
+    [2,1,2,2,1],
+    [0,0,1,0,1],
+    [1,0,2,0,1],
+    [0,1,1,1,-1]
+  ],
+  "parameters": {
+    "temperature_initial": 30.0,
+    "temperature_target": 0.0000001
+  },
+  "outputs": {
+    "averaged_spins": True,
+    "averaged_energy": True
+  }
+}
+
+
+
+
+
+
+
+data = urllib.parse.urlencode(params)
+
+
+
+
 headers = {'Authorization': 'Bearer '+ access_token}
 
 
