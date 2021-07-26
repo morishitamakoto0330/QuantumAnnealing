89d1f543e578190223b43f9b2dfa4b30c2053e94
morishitamakoto0330 <morishita0330@gmail.com>
2021-07-26 13:21:49 +0900

    amplifyに変更

--- CMOSAnnealing/VertexCovering/solve.py
+++ CMOSAnnealing/VertexCovering/solve.py
@@ -1,44 +1,18 @@
-import urllib.request
-import urllib.parse
-import json
-
-import vertex_covering as vc
 from secret import get_token
 
-# トークン設定
-access_token = get_token()
-headers = {'Authorization': 'Bearer '+ access_token}
-url = 'https://annealing-cloud.com/api/v2/solve'
+from amplify import BinaryPoly, gen_symbols
+from amplify.client import HitachiClient
 
-# 頂点被覆問題の設定
-N = 2
-model = vc.set(N)
+q = gen_symbols(BinaryPoly, 512, 512)
+f = -2 * q[0][0] * q[1][1] - q[0][0] - q[1][1] + 1
 
-# APIに渡すパラメータ
-params = {
-	"type": 4,
-  "num_executions": 1,
-  "model": model,
-  "parameters": {
-    "temperature_initial": 10.0,
-    "temperature_target": 0.01
-  },
-  "outputs": {
-    "averaged_spins": True,
-    "averaged_energy": True
-  }
-}
+client = HitachiClient()
+client.token = get_token()
+client.parameters.temperature_num_steps = 10
+client.parameters.temperature_step_length = 100
+client.parameters.temperature_initial = 100.0
+client.parameters.temperature_target = 0.02
 
-
-# API呼び出し
-data = urllib.parse.urlencode(params)
-data = data.encode('ascii')
-req = urllib.request.Request(url, json.dumps(params).encode(), headers, 'POST')
-
-# CMOSアニーリングマシン実行結果受け取り
-with urllib.request.urlopen(req) as response:
-	json_data = json.loads(response.read())
-	print(json.dumps(json_data, indent=2))
-
+result = client.solve(f)
 
 
