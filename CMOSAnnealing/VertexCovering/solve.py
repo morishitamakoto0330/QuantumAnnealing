184012aec23f72d7bdcd3ab57fefd0e2a6970036
morishitamakoto0330 <morishita0330@gmail.com>
2021-07-27 16:42:25 +0900

    重み変更の2重ループ作成

--- CMOSAnnealing/VertexCovering/solve.py
+++ CMOSAnnealing/VertexCovering/solve.py
@@ -5,8 +5,7 @@ from amplify.constraint import greater_equal
 from amplify.client import HitachiClient
 
 # 問題設定（正方格子グラフにおける頂点被覆）
-N = 4         # 問題サイズ
-w1 = w2 = 1.0 # 初期値=1.0
+N = 2         # 問題サイズ
 q = gen_symbols(BinaryPoly, N, N)
 
 # コスト関数（w1 で調整）
@@ -15,21 +14,32 @@ cost_function = sum([sum_poly(q[i]) for i in range(N)])
 # 制約（w2 で調整）
 constraint_x = sum([greater_equal(q[i][j] + q[i + 1][j], 1) for i in range(N - 1) for j in range(N)])
 constraint_y = sum([greater_equal(q[i][j] + q[i][j + 1], 1) for i in range(N) for j in range(N - 1)])
-constraint = w2 * (constraint_x + constraint_y)
+constraint = constraint_x + constraint_y
 
-# 最終的なエネルギー関数
-energy_function = w1*cost_function + w2*constraint
-
+# クライアント設定
 client = HitachiClient()
 client.token = get_token()
+client.parameters.num_executions = 1
 client.parameters.temperature_num_steps = 100
 client.parameters.temperature_step_length = 100
 client.parameters.temperature_initial = 100.0
 client.parameters.temperature_target = 0.02
 
 solver = Solver(client)
-result = solver.solve(energy_function)
 
-print([s.values for s in result])
+# 重み変更して複数回アニーリング
+w_init = 0.1
+num = 10
+for i in range(num):
+    w1 = i * w_init
+    for j in range(num):
+        w2 = j * w_init
+        # 最終的なエネルギー関数
+        energy_function = w1*cost_function + w2*constraint
+        # 解く
+        result = solver.solve(energy_function)
+        # 結果出力
+        print('(w1, w2)=({0}, {1})'.format(w1, w2))
+        print([s.values for s in result])
 
 
