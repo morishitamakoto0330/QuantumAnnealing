258f4e01b2d42018dc4107e6e1f14e574e5a5813
morishitamakoto0330 <morishita0330@gmail.com>
2021-08-03 13:10:20 +0900

    結果出力

--- CMOSAnnealing/VertexCovering/solve.py
+++ CMOSAnnealing/VertexCovering/solve.py
@@ -9,10 +9,10 @@ import utils
 N = 2         # 問題サイズ
 q = gen_symbols(BinaryPoly, N, N)
 
-# コスト関数（w1 で調整）
+# コスト関数（w_b で調整）
 cost_function = sum([sum_poly(q[i]) for i in range(N)])
 
-# 制約（w2 で調整）
+# 制約（w_a で調整）
 constraint_x = sum([greater_equal(q[i][j] + q[i + 1][j], 1) for i in range(N - 1) for j in range(N)])
 constraint_y = sum([greater_equal(q[i][j] + q[i][j + 1], 1) for i in range(N) for j in range(N - 1)])
 constraint = constraint_x + constraint_y
@@ -34,11 +34,11 @@ num_steps = 10       # 重み変更回数
 num_execution = 100  # アニーリング実行回数
 
 for i in range(1, num_steps + 1):
-    w1 = i * w_step
+    w_a = i * w_step
     for j in range(1, num_steps + 1):
-        w2 = j * w_step
+        w_b = j * w_step
         # 最終的なエネルギー関数
-        energy_function = w1*cost_function + w2*constraint
+        energy_function = w_a*constraint + w_b*cost_function
 
         num_optimum = 0      # 最適解が導かれた回数
         for _ in range(num_execution):
@@ -51,6 +51,7 @@ for i in range(1, num_steps + 1):
             #print('(w1, w2)=({0}, {1})'.format(w1, w2))
             #print('{0}'.format(result_str))
             #print('{0}'.format(result_is_optimum))
-        print('最適回答率{0}[%]'.format((num_optimum / num_execution) * 100))
+        percentage_of_optimum = (num_optimum / num_execution) * 100
+        print('N={0}, (w_a, w_b)=({1}, {2}): 最適回答率{3}[%]'.format(N, w_a, w_b, percentage_of_optimum))
 
 
