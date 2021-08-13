f469c2939733cc6f9107f86cb507639be9a8b044
morishitamakoto0330 <morishita0330@gmail.com>
2021-08-14 01:58:29 +0900

    問題サイズN変更

--- CMOSAnnealing/VertexCovering/solve.py
+++ CMOSAnnealing/VertexCovering/solve.py
@@ -7,68 +7,69 @@ import utils
 
 import time
 
-# 問題設定（正方格子グラフにおける頂点被覆）
-N = 2         # 問題サイズ
-q = gen_symbols(BinaryPoly, N, N)
+# 問題サイズ N=2,4,6,8,10,12,14,16 で実行
+for N in range(2, 16, 2):
+    # 問題設定（正方格子グラフにおける頂点被覆）
+    q = gen_symbols(BinaryPoly, N, N)
 
-# コスト関数（w_b で調整）
-cost_function = sum([sum_poly(q[i]) for i in range(N)])
+    # コスト関数（w_b で調整）
+    cost_function = sum([sum_poly(q[i]) for i in range(N)])
 
-# 制約（w_a で調整）
-constraint_x = sum([greater_equal(q[i][j] + q[i + 1][j], 1) for i in range(N - 1) for j in range(N)])
-constraint_y = sum([greater_equal(q[i][j] + q[i][j + 1], 1) for i in range(N) for j in range(N - 1)])
-constraint = constraint_x + constraint_y
+    # 制約（w_a で調整）
+    constraint_x = sum([greater_equal(q[i][j] + q[i + 1][j], 1) for i in range(N - 1) for j in range(N)])
+    constraint_y = sum([greater_equal(q[i][j] + q[i][j + 1], 1) for i in range(N) for j in range(N - 1)])
+    constraint = constraint_x + constraint_y
 
-# クライアント設定
-client = HitachiClient()
-client.token = get_token()
-client.parameters.num_executions = 1
-client.parameters.outputs.execution_time = True
-client.parameters.temperature_num_steps = 10
-client.parameters.temperature_step_length = 100
-client.parameters.temperature_initial = 100.0
-client.parameters.temperature_target = 0.02
+    # クライアント設定
+    client = HitachiClient()
+    client.token = get_token()
+    client.parameters.num_executions = 1
+    client.parameters.outputs.execution_time = True
+    client.parameters.temperature_num_steps = 10
+    client.parameters.temperature_step_length = 100
+    client.parameters.temperature_initial = 100.0
+    client.parameters.temperature_target = 0.02
 
-solver = Solver(client)
+    solver = Solver(client)
 
-# 重み変更してアニーリング実行
-w_step = 0.1         # 重み変更幅
-num_steps = 1       # 重み変更回数
-num_execution = 1     # アニーリング実行回数
+    # 重み変更してアニーリング実行
+    w_step = 0.01           # 重み変更幅
+    num_steps = 500         # 重み変更回数
+    num_execution = 100     # アニーリング実行回数
 
-print('N, w_a, w_b, optimal_answer_percentage[%], time[s]')
-# 結果出力用ファイルを開く
-f = open('result.txt', 'w')
+    print('N, w_a, w_b, optimal_answer_percentage[%], time[s]')
+    # 結果出力用ファイルを開く
+    f = open('result_{0}.txt'.format(N), 'w')
 
-for i in range(1, num_steps + 1):
-    w_a = i * w_step
-    for j in range(1, num_steps + 1):
-        w_b = j * w_step
-        # 最終的なエネルギー関数
-        energy_function = w_a*constraint + w_b*cost_function
+    for i in range(1, num_steps + 1):
+        w_a = i * w_step
+        for j in range(1, num_steps + 1):
+            w_b = j * w_step
+            # 最終的なエネルギー関数
+            energy_function = w_a*constraint + w_b*cost_function
 
-        num_optimum = 0      # 最適解が導かれた回数
-        sum_exe_time = 0.0   # 実行時間の総和
-        for _ in range(num_execution):
-            # 通信時間を含む計算実行時間の計測
-            start_time = time.perf_counter()
-            result = solver.solve(energy_function)
-            end_time = time.perf_counter()
+            num_optimum = 0      # 最適解が導かれた回数
+            sum_exe_time = 0.0   # 実行時間の総和
+            for _ in range(num_execution):
+                # 通信時間を含む計算実行時間の計測
+                start_time = time.perf_counter()
+                result = solver.solve(energy_function)
+                end_time = time.perf_counter()
 
-            sum_exe_time += end_time - start_time
-            # 結果出力
-            _, result_is_optimum = utils.print_solver_result(N, result)
-            if result_is_optimum:
-                num_optimum += 1
-        # 最適回答率
-        percentage_of_optimum = (num_optimum / num_execution) * 100
-        # 実行時間
-        exe_time = sum_exe_time / num_execution
-        result_str = '{0}, {1}, {2}, {3}, {4}\n'.format(N, w_a, w_b, percentage_of_optimum, exe_time)
-        # ログ出力
-        print(result_str)
-        # ファイル書き込み
-        f.write(result_str)
-# 結果出力用ファイルを閉じる
-f.close()
+                sum_exe_time += end_time - start_time
+                # 結果出力
+                _, result_is_optimum = utils.print_solver_result(N, result)
+                if result_is_optimum:
+                    num_optimum += 1
+            # 最適回答率
+            percentage_of_optimum = (num_optimum / num_execution) * 100
+            # 実行時間
+            exe_time = sum_exe_time / num_execution
+            result_str = '{0}, {1}, {2}, {3}, {4}\n'.format(N, w_a, w_b, percentage_of_optimum, exe_time)
+            # ログ出力
+            print(result_str)
+            # ファイル書き込み
+            f.write(result_str)
+    # 結果出力用ファイルを閉じる
+    f.close()
 
